6ecec67056684a719fbe1389df1f499631f1a133
Benja868 <benjamin.araya28@inacapmail.cl>
2025-08-22 15:01:54 -0400

    creacion de las clases, falta crear y testear admin

--- monitoreo/dispositivos/admin.py
+++ monitoreo/dispositivos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Dispositivo
 
-# Register your models here.
+@admin.register(Dispositivo)
+class DispositivoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "consumo_maximo", "categoria", "zona")
+    search_fields = ("nombre", "categoria", "zona")
--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,16 +1,43 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
-    descipcion = models.TextField(blank=True, null=True)
-    
+
     def __str__(self):
-        return super().__str__()
+        return self.nombre
+
+
+class Zona(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
 
 class Dispositivo(models.Model):
     nombre = models.CharField(max_length=100)
     consumo = models.IntegerField()
-    estado = models.BooleanField(default=True)
-    
+    categoria = models.CharField(max_length=50, default="General")
+    zona = models.CharField(max_length=50, default="Sin zona")
+
     def __str__(self):
-        return super().__str__()
+        return self.nombre
+
+
+class Medicion(models.Model):
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    consumo = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.dispositivo.nombre} - {self.consumo}W ({self.fecha})"
+
+
+class Alerta(models.Model):
+    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE)
+    mensaje = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Alerta: {self.dispositivo.nombre} - {self.mensaje[:20]}..."
