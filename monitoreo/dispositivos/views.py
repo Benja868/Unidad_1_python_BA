56109b9a06d99c04b9909163dd652646d234e7ff
CarlosCortez7 <carlos.cortez20@inacapmail.cl>
2025-08-22 23:21:11 -0400

    Implementacion del codigo con base de datos y marcador de limite de W

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
-
-# Datos simulados de dispositivos
-dispositivos = [
-    {"id": 1, "nombre": "Refrigerador", "consumo": 450, "limite": 500},
-    {"id": 2, "nombre": "Aire Acondicionado", "consumo": 1200, "limite": 1000},
-    {"id": 3, "nombre": "Computadora", "consumo": 300, "limite": 400},
-    {"id": 4, "nombre": "Televisor", "consumo": 600, "limite": 700},
-]
+from dispositivos.models import Dispositivo
 
 def index(request):
     return render(request, "index.html")
 
 def panel(request):
+    dispositivos = Dispositivo.objects.all()
     contexto = {"dispositivos": dispositivos}
     return render(request, "panel.html", contexto)
 
